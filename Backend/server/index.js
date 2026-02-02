6e93b747d9e1c96affb6c96b667e472abd900b9c
Sabbarigrishan <sabbari26504@gmail.com>
2026-02-03 00:28:15 +0530

    Update index.js

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -28,16 +28,14 @@ dotenv.config();
 // Email Transporter
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
-  },
-  tls: {
-    rejectUnauthorized: false
   }
 });
+
 
 
 
